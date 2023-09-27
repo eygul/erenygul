95e3db562c5a93d7b563ea2bd2405826ba112bef
gule <erenygedek02@gmail.com>
2023-09-27 10:14:52 -0400

    add resume

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
+    
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
